4608ebf760b556da84c55243ace59008a04f56dd
Kamal Nasser <hello@kamal.io>
2020-04-30 21:24:30 +0300

    refactor a little?

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/skratchdot/open-golang/open"
 	"github.com/urfave/cli/v2"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -42,135 +38,57 @@ func main() {
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
 
 func rootCmd(c *cli.Context) error {
 	if c.Args().Len() != 2 {
-		fmt.Printf("usage: kubectl dobrowse <type> <name>\n\n\texample: kubectl dobrowse service main-load-balancer")
-		return nil
+		return fmt.Errorf("usage: kubectl dobrowse <type> <name>\n\n\texample: kubectl dobrowse service main-load-balancer")
 	}
 
 	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: c.String("kubeconfig")},
-		&clientcmd.ConfigOverrides{})
-	clientConfig, err := kubeConfig.ClientConfig()
+		&clientcmd.ConfigOverrides{},
+	)
 
-	resourceType, resourceName := c.Args().Get(0), c.Args().Get(1)
-	resourceNamespace := c.String("namespace")
-	if resourceNamespace == "" {
-		resourceNamespace, _, _ = kubeConfig.Namespace()
+	namespace := c.String("namespace")
+	if namespace == "" {
+		namespace, _, _ = kubeConfig.Namespace()
 	}
-	fmt.Printf("opening %s %s (namespace %s)\n", resourceType, resourceName, resourceNamespace)
+	typ := c.Args().Get(0)
+	name := c.Args().Get(1)
 
+	resource := ParseResource(typ)
+	if resource == nil {
+		return fmt.Errorf("unsupported resource type %s", typ)
+	}
+
+	fmt.Printf("opening %s %s (namespace %s)\n", typ, name, namespace)
+	return browse(kubeConfig, resource, namespace, name)
+}
+
+func browse(kubeConfig clientcmd.ClientConfig, resource Resource, namespace, name string) error {
+	clientConfig, err := kubeConfig.ClientConfig()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
 	clientset, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
-	path, err := getCloudPath(clientset, resourceNamespace, resourceType, resourceName)
+	path, err := resource.CloudPath(context.Background(), clientset, namespace, name)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 
-	open.Run(fmt.Sprintf("%s%s", cloudBase, path))
+	url := fmt.Sprintf("%s%s", cloudBase, path)
+	open.Run(url)
 
 	return nil
-}
-
-func getCloudPath(clientset *kubernetes.Clientset, resourceNamespace, resourceType, resourceName string) (string, error) {
-	switch resourceType {
-	case "node":
-		fallthrough
-	case "nodes":
-		fallthrough
-	case "no":
-		node, err := clientset.CoreV1().Nodes().Get(context.TODO(), resourceName, metav1.GetOptions{})
-		if err != nil {
-			return "", err
-		}
-
-		if !strings.HasPrefix(node.Spec.ProviderID, "digitalocean://") {
-			return "", fmt.Errorf("node %s is not a DigitalOcean-provisioned node", resourceName)
-		}
-
-		id := strings.TrimPrefix(node.Spec.ProviderID, "digitalocean://")
-		return fmt.Sprintf("droplets/%s", id), nil
-
-	case "service":
-		fallthrough
-	case "services":
-		fallthrough
-	case "svc":
-		svc, err := clientset.CoreV1().Services(resourceNamespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-		if err != nil {
-			return "", err
-		}
-
-		svcType := svc.Spec.Type
-		if svcType != corev1.ServiceTypeLoadBalancer {
-			return "", fmt.Errorf("Service %s is of the type %s, not a LoadBalancer", resourceName, svcType)
-		}
-
-		id, ok := svc.Annotations["kubernetes.digitalocean.com/load-balancer-id"]
-		if !ok {
-			return "", fmt.Errorf("annotation kubernetes.digitalocean.com/load-balancer-id not found on service")
-		}
-
-		return fmt.Sprintf("networking/load_balancers/%s", id), nil
-
-	case "persistentvolume":
-		fallthrough
-	case "persistentvolumes":
-		fallthrough
-	case "pv":
-		pv, err := clientset.CoreV1().PersistentVolumes().Get(context.TODO(), resourceName, metav1.GetOptions{})
-		if err != nil {
-			return "", err
-		}
-
-		pvClass := pv.Spec.StorageClassName
-		if pvClass != "do-block-storage" {
-			return "", fmt.Errorf("PersistentVolume %s is not a DigitalOcean Block Storage Volume. Storage class must be %s but got %s", "do-block-storage", pvClass)
-		}
-
-		fmt.Printf("PersistentVolume name: %s\n", pv.Name)
-		return "volumes", nil
-
-	case "persistentvolumeclaim":
-		fallthrough
-	case "persistentvolumeclaims":
-		fallthrough
-	case "pvc":
-		pvc, err := clientset.CoreV1().PersistentVolumeClaims(resourceNamespace).Get(context.TODO(), resourceName, metav1.GetOptions{})
-		if err != nil {
-			return "", err
-		}
-
-		pvcPhase := pvc.Status.Phase
-		if pvcPhase != corev1.ClaimBound {
-			return "", fmt.Errorf("PersistentVolumeClaim %s is not bound to a PersistentVolume. Got phase %s", resourceName, pvcPhase)
-		}
-
-		pvClass := *pvc.Spec.StorageClassName
-		if pvClass != "do-block-storage" {
-			return "", fmt.Errorf("PersistentVolume %s is not a DigitalOcean Block Storage Volume. Storage class must be %s but got %s", resourceName, "do-block-storage", pvClass)
-		}
-
-		fmt.Printf("PersistentVolume name: %s\n", pvc.Spec.VolumeName)
-		return "volumes", nil
-
-	default:
-		return "", fmt.Errorf("unknown resource type %s", resourceType)
-	}
 }
 
 func defaultKubeconfigPath() string {
---
+++ resources.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const nodeIDPrefix = "digitalocean://"
+const lbaasAnnotation = "kubernetes.digitalocean.com/load-balancer-id"
+
+type Resource interface {
+	CloudPath(context.Context, *kubernetes.Clientset, string, string) (string, error)
+}
+
+type Cluster struct{}
+type Node struct{}
+type Service struct{}
+type PersistentVolume struct{}
+type PersistentVolumeClaim struct{}
+
+var (
+	_ Resource = &Cluster{}
+	_ Resource = &Node{}
+	_ Resource = &Service{}
+	_ Resource = &PersistentVolume{}
+	_ Resource = &PersistentVolumeClaim{}
+)
+
+var namesMap = map[string]Resource{
+	"cluster":                &Cluster{},
+	"nodes":                  &Node{},
+	"node":                   &Node{},
+	"no":                     &Node{},
+	"services":               &Service{},
+	"service":                &Service{},
+	"svc":                    &Service{},
+	"persistentvolume":       &PersistentVolume{},
+	"persistentvolumes":      &PersistentVolume{},
+	"pv":                     &PersistentVolume{},
+	"persistentvolumeclaim":  &PersistentVolumeClaim{},
+	"persistentvolumeclaims": &PersistentVolumeClaim{},
+	"pvc":                    &PersistentVolumeClaim{},
+}
+
+func ParseResource(name string) Resource {
+	if r, ok := namesMap[name]; ok {
+		return r
+	}
+
+	return nil
+}
+
+func (c *Cluster) CloudPath(ctx context.Context, clientset *kubernetes.Clientset, namespace, name string) (string, error) {
+	return "", fmt.Errorf("not implemented")
+}
+
+func (n *Node) CloudPath(ctx context.Context, clientset *kubernetes.Clientset, namespace, name string) (string, error) {
+	node, err := clientset.CoreV1().Nodes().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	if !strings.HasPrefix(node.Spec.ProviderID, nodeIDPrefix) {
+		return "", fmt.Errorf("node %s is not a DigitalOcean-provisioned node", name)
+	}
+
+	id := strings.TrimPrefix(node.Spec.ProviderID, nodeIDPrefix)
+	return fmt.Sprintf("droplets/%s", id), nil
+}
+
+func (s *Service) CloudPath(ctx context.Context, clientset *kubernetes.Clientset, namespace, name string) (string, error) {
+	svc, err := clientset.CoreV1().Services(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	svcType := svc.Spec.Type
+	if svcType != corev1.ServiceTypeLoadBalancer {
+		return "", fmt.Errorf("Service %s is of the type %s, not a LoadBalancer", name, svcType)
+	}
+
+	id, ok := svc.Annotations[lbaasAnnotation]
+	if !ok {
+		return "", fmt.Errorf("annotation %s not found on service", lbaasAnnotation)
+	}
+
+	return fmt.Sprintf("networking/load_balancers/%s", id), nil
+}
+
+func (pv *PersistentVolume) CloudPath(ctx context.Context, clientset *kubernetes.Clientset, namespace, name string) (string, error) {
+	pvObj, err := clientset.CoreV1().PersistentVolumes().Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	pvClass := pvObj.Spec.StorageClassName
+	if pvClass != "do-block-storage" {
+		return "", fmt.Errorf("PersistentVolume %s is not a DigitalOcean Block Storage Volume. Storage class must be %s but got %s", "do-block-storage", pvClass)
+	}
+
+	// TODO: extract os.Stdout
+	fmt.Fprintf(os.Stdout, "PersistentVolume name: %s\n", pvObj.Name)
+	return "volumes", nil
+}
+
+func (pvc *PersistentVolumeClaim) CloudPath(ctx context.Context, clientset *kubernetes.Clientset, namespace, name string) (string, error) {
+	pvcObj, err := clientset.CoreV1().PersistentVolumeClaims(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	pvcPhase := pvcObj.Status.Phase
+	if pvcPhase != corev1.ClaimBound {
+		return "", fmt.Errorf("PersistentVolumeClaim %s is not bound to a PersistentVolume. Got phase %s", name, pvcPhase)
+	}
+
+	pvClass := *pvcObj.Spec.StorageClassName
+	if pvClass != "do-block-storage" {
+		return "", fmt.Errorf("PersistentVolume %s is not a DigitalOcean Block Storage Volume. Storage class must be %s but got %s", name, "do-block-storage", pvClass)
+	}
+
+	fmt.Fprintf(os.Stdout, "PersistentVolume name: %s\n", pvcObj.Spec.VolumeName)
+	return "volumes", nil
+}
